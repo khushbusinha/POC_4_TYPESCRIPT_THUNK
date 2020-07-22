6c9302f4e4e3e0628f5f09d0f1d87ab32436e1b0
Kumari Sinha <kumari.sinha@in.abb.com>
2020-07-22 17:53:11 +0530

    redux-thunk implementation

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Counter from "./components/Counter";
+import FunFact from "./components/FunFact";
+
+function App() {
+    return (
+        <div>
+            <Counter />
+            <br/>
+            <FunFact/>
+        </div>
+    );
+}
+
+export default App;
